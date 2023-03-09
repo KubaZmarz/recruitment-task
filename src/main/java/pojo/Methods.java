c6ca920201d82b0987b861b8f7774fa9ed5c5df9
KubaZmarz <jakub.zmarz@zazoon.com>
2023-03-09 14:46:13 +0100

    forgotten commit

--- src/main/java/pojo/Methods.java
+++ src/main/java/pojo/Methods.java
@@ -1,8 +1,11 @@
 package pojo;
 
-import io.restassured.http.ContentType;
-import org.json.JSONObject;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 
 public class Methods {
@@ -24,10 +27,59 @@ public class Methods {
                 .getString("token");
     }
 
-    public static void createBooking() {
-        JSONObject payload = new JSONObject();
-        payload.put("firstName", "Test");
-        payload.put("lastName", "Test");
+    public static Response getBooking(String token, String bookingId) {
+        return RestAssured
+                .given()
+                .accept(ContentType.JSON)
+                .cookie(token)
+                .queryParam(bookingId)
+                .when()
+                .get("/booking");
+    }
 
+    public static Response createBooking(String token) { // normally I'd pass a model object with payload, sth like CreateBookingRQ but for those tests I'll hardcode data
+        JSONObject payload = new JSONObject();
+        payload.put("firstName", "Kuba");
+        payload.put("lastName", "Test");
+        payload.put("totalprice", 100);
+        payload.put("depositpaid", true);
+        payload.put("bookingdates", new HashMap<String, String>() {{
+            put("checkin", "2013-02-23");
+            put("checkout", "2014-10-23");
+        }});
+        payload.put("additionalneeds", "breakfast");
+
+        return RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .cookie(token)
+                .body(payload)
+                .post("/booking");
+    }
+
+    public static Response updateBooking(String token, String bookingId) {
+        JSONObject payload = new JSONObject();
+        payload.put("bookingid", bookingId);
+        payload.put("firstName", "Test");
+        payload.put("totalprice", 200);
+        payload.put("additionalneeds", "dinner");
+
+        return RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .cookie(token)
+                .body(payload)
+                .patch("/booking");
+    }
+
+    public static Response deleteBooking(String token, String bookingId) {
+        return RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .cookie(token)
+                .queryParam(bookingId)
+                .delete("/booking");
     }
 }
--- src/test/java/Exercise2.java
+++ src/test/java/Exercise2.java
@@ -1,21 +1,47 @@
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pojo.Methods;
 
-import java.io.FileInputStream;
-import java.util.Properties;
-
 public class Exercise2 {
+
+    private String token;
 
     @BeforeTest
     public void setup() {
         RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        token = "token=" + (Methods.getAuthToken("admin", "password123"));
     }
 
     @Test
     public void test() {
-        Methods.getAuthToken("admin", "password123");
+        Response createBookingRS = Methods.createBooking(token);
+        createBookingRS.then().statusCode(HttpStatus.SC_OK).log(); // verify and log
+
+        String jsonBody = createBookingRS.then().extract().body().toString();
+        JSONObject json = new JSONObject(jsonBody);
+        String id = json.getString("bookingid");
+
+        Methods.getBooking(token, id)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .log();
+
+        Methods.updateBooking(token, id)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .log();
+
+        Methods.deleteBooking(token, id)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED)
+                .log();
     }
 
 }
